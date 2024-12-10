ddcced834b783128e1d2c960f7bb094bd947cdf4
yuyanjun <yuyanjun@zego.im>
2024-12-10 19:12:36 +0800

    update version v4.16.15

--- lib/src/call.dart
+++ lib/src/call.dart
@@ -151,7 +151,7 @@ class _ZegoUIKitPrebuiltCallState extends State<ZegoUIKitPrebuiltCall>
     return isAllEntered;
   }
 
-  String get version => "4.16.14";
+  String get version => "4.16.15";
 
   @override
   void initState() {
--- lib/src/invitation/service.dart
+++ lib/src/invitation/service.dart
@@ -137,7 +137,7 @@ part 'mixins/private/invitation.dart';
 /// ```
 class ZegoUIKitPrebuiltCallInvitationService
     with ZegoCallInvitationServicePrivate, ZegoCallInvitationServiceAPI {
-  String get version => '4.16.14';
+  String get version => '4.16.15';
 
   bool get isInit => private._isInit;
 
