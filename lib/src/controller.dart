694a7bc31f6f0411017ac6b7302b079655c6f030
yuyanjun <yuyanjun@zego.im>
2025-07-19 01:07:34 +0800

    v4.17.7

--- lib/src/controller.dart
+++ lib/src/controller.dart
@@ -80,7 +80,7 @@ class ZegoUIKitPrebuiltCallController
         ZegoCallControllerPrivate {
   factory ZegoUIKitPrebuiltCallController() => instance;
 
-  String get version => "4.17.6";
+  String get version => "4.17.7";
 
   /// This function is used to end the current call.
   ///
