5d079af855b0ac8d7d0613cb2dc43fd207235d03
yuyanjun <yuyanjun@zego.im>
2026-05-09 11:55:24 +0800

    lack of import

--- lib/src/controller.dart
+++ lib/src/controller.dart
@@ -88,7 +88,7 @@ class ZegoUIKitPrebuiltCallController
         ZegoCallControllerPrivate {
   factory ZegoUIKitPrebuiltCallController() => instance;
 
-  String get version => "4.23.2";
+  String get version => "4.23.3";
 
   /// This function is used to end the current call.
   ///
